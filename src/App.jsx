f79f30464c3eb978ef76aca6b7f52d58966007cc
Ed Gonzales <edgonzales6@gmail.com>
2023-11-09 14:54:21 -0800

    Components setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 // import { useState } from 'react'
 import './App.css'
+import GiphyShow from './components/GiphShow/GiphShow'
+import SearchBar from './components/SearchBar/SearchBar'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
+      <SearchBar />
+      <GiphyShow />
      
     </>
   )
