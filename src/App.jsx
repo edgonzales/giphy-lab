3c591e57b843eb89a532f91f1ca284f1cc168e5d
Ed Gonzales <edgonzales6@gmail.com>
2023-11-09 16:17:02 -0800

    Giphy API setup

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,19 @@ function App() {
 
   useEffect(() => {
     console.log('USE EFFECT IS RUNNING!')
+    const endpoint = "http://api.giphy.com/v1/gifs/search?q=run&api_key=ZeCa7IAoWGQSOizaBTCKGrhWJMWZVkxK&limit=1";
 
-
-  })
+    async function getGyphy() {
+      try {
+        const response = await fetch(endpoint);
+        const body = await response.json(); 
+        console.log(body);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getGyphy();
+  }, []);
 
   return (
     <>
